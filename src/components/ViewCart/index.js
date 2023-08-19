c5f102952f1a9eaa3901c667277146f8521f3f97
Igor Zupunski <ipatheword@live.com>
2023-08-19 11:45:35 +0200

    applied class names

--- src/components/ViewCart/index.js
+++ src/components/ViewCart/index.js
@@ -11,28 +11,28 @@ const ViewCart = () => {
 
     return (
         <div className="view-cart">
-            <h2>
+            <h2 className="cart-heading">
                 Your Shopping Cart
             </h2>
             {cartItems.length === 0 ? (
-                <p> Your cart is empty </p>
+                <p className="empty-cart-message"> Your cart is empty </p>
             ) : (
                 <div className="cart-items">
                     {cartItems.map((item) => {
                         return (
                             <div key={item.product.id} className="cart-item">
                                 <div className="item-details">
-                                    <h3>{item.product.name}</h3>
-                                    <p>Quantity: {item.quantity}</p>
-                                    <p>Price: ${item.product.price.toFixed(2)}</p>
+                                    <h3 className="item-name">{item.product.name}</h3>
+                                    <p className="item-quantity">Quantity: {item.quantity}</p>
+                                    <p className="item-price">Price: ${item.product.price.toFixed(2)}</p>
                                 </div>
                             </div>
                         );
                     })}
 
                     <div className="cart-total">
-                        <h3>Total: </h3>
-                        <p>${calculateTotalPrice().toFixed(2)}</p>
+                        <h3 className="total-heading">Total: </h3>
+                        <p className="total-amount">${calculateTotalPrice().toFixed(2)}</p>
                     </div>
                 </div>
             )}
