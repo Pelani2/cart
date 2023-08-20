f6faf6c26f913374367113048b01d3ad4d1166d6
Igor Zupunski <ipatheword@live.com>
2023-08-20 12:12:15 +0200

    finished shipping form

--- src/components/Forms/Shipping/index.js
+++ src/components/Forms/Shipping/index.js
@@ -23,101 +23,103 @@ const ShippingForm = ({ onNext }) => {
     };
 
     return (
-        <div className="shipping-form">
-            <h2>Shipping Form</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-group">
-                    <label htmlFor="firstName">First name:</label>
-                    <Controller 
-                        name="firstName"
-                        control={control}
-                        render={({ field }) => (
-                            <input {...field} type="text" />
-                        )}
-                    />
-                    <p className="error-message">
-                        {errors.firstName?.message}
-                    </p>
-                </div>
+        <div className="shipping-form__container">
+            <div className="shipping-form">
+                <h2>Shipping Form</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <label htmlFor="firstName">First name:</label>
+                        <Controller 
+                            name="firstName"
+                            control={control}
+                            render={({ field }) => (
+                                <input {...field} type="text" />
+                            )}
+                            />
+                        <p className="error-message">
+                            {errors.firstName?.message}
+                        </p>
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="lastName">
-                        Last name:
-                    </label>
-                    <Controller 
-                        name="lastName"
-                        control={control}
-                        render={({ field }) => (
-                            <input {...field} type="text"/>
-                        )}
-                    />
-                    <p className="error-message">
-                        {errors.lastName?.message}
-                    </p>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="address">
-                        Address: 
-                    </label>
-                    <Controller 
-                        name="address"
-                        control={control}
-                        render={({ field }) => (
-                            <input {...field} type="text" />
-                        )}
-                    />
-                    <p className="error-message">
-                        {errors.address?.message}
-                    </p>
-                </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">
+                            Last name:
+                        </label>
+                        <Controller 
+                            name="lastName"
+                            control={control}
+                            render={({ field }) => (
+                                <input {...field} type="text"/>
+                                )}
+                                />
+                        <p className="error-message">
+                            {errors.lastName?.message}
+                        </p>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="address">
+                            Address: 
+                        </label>
+                        <Controller 
+                            name="address"
+                            control={control}
+                            render={({ field }) => (
+                                <input {...field} type="text" />
+                                )}
+                                />
+                        <p className="error-message">
+                            {errors.address?.message}
+                        </p>
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="city">
-                        City:
-                    </label>
-                    <Controller 
-                        name="city"
-                        control={control}
-                        render={({ field }) => (
-                            <input {...field} type="text" />
-                        )}
-                    />
-                    <p className="error-message">
-                        {errors.city?.message}
-                    </p>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="postalCode">
-                        Postal code:
-                    </label>
-                    <Controller 
-                        name="postalCode"
-                        control={control}
-                        render={({ field }) => (
-                            <input {...field} type="text" />
-                        )}
-                    />
-                    <p className="error-message">
-                        {errors.postalCode?.message}
-                    </p>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="country">
-                        Country:
-                    </label>
-                    <Controller 
-                        name="country"
-                        control={control}
-                        render={({ field }) => (
-                            <input {...field} type="text" />
-                        )}
-                    />
-                    <p className="error-message">
-                        {errors.country?.message}
-                    </p>
-                </div>
-                <button type="submit">Next</button>
-            </form>
+                    <div className="form-group">
+                        <label htmlFor="city">
+                            City:
+                        </label>
+                        <Controller 
+                            name="city"
+                            control={control}
+                            render={({ field }) => (
+                                <input {...field} type="text" />
+                                )}
+                                />
+                        <p className="error-message">
+                            {errors.city?.message}
+                        </p>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="postalCode">
+                            Postal code:
+                        </label>
+                        <Controller 
+                            name="postalCode"
+                            control={control}
+                            render={({ field }) => (
+                                <input {...field} type="text" />
+                                )}
+                                />
+                        <p className="error-message">
+                            {errors.postalCode?.message}
+                        </p>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="country">
+                            Country:
+                        </label>
+                        <Controller 
+                            name="country"
+                            control={control}
+                            render={({ field }) => (
+                                <input {...field} type="text" />
+                            )}
+                            />
+                        <p className="error-message">
+                            {errors.country?.message}
+                        </p>
+                    </div>
+                    <button type="submit">Next</button>
+                </form>
+            </div>
         </div>
     );
 };
